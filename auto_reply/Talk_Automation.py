3dcb904bf3113cc920d700476d2194021e9b4077
Eddie Tseng <35317612+eddietseng1129@users.noreply.github.com>
2020-05-24 14:14:17 -0400

    Update chatbot

--- auto_reply/Talk_Automation.py
+++ auto_reply/Talk_Automation.py
@@ -1,19 +1,26 @@
 import bs4
 import time
 import requests
+import collections
 from selenium import webdriver
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 from datetime import datetime
 from selenium.webdriver.common.keys import Keys
+from create_chatbot import create_chatbot
 
 class Talk_Automation():
-	def __init__(self, url, count, prev_message, time_not_response):
+	def __init__(self, url, count, prev_message, time_not_response, max_people, set_robot=True):
 		self.browser = webdriver.Chrome(executable_path=r'/usr/local/bin/chromedriver')
 		self.browser.get(url)
 		self.count = count
+		self.max_people = max_people
 		self.prev_message = prev_message
+		self.dialog = collections.defaultdict(set)
 		self.time_not_response = time_not_response
+		self.set_robot = set_robot
+		if self.set_robot:
+			self.robot = create_chatbot('conversations') 
 	
 	def start(self):
 		self.browser.find_element_by_css_selector('.start').click()
@@ -33,7 +40,7 @@ class Talk_Automation():
 			if someone_leave.find_element_by_css_selector('.message-content').text == '對話已結束，請點我重新配對聊天':
 				someone_leave.find_element_by_css_selector('.message-content').click()
 				print("She/He just leave, finding new one......")
-				print(self.count)
+				print(str(self.count) + ' / ' + str(self.max_people))
 			self.count += 1
 			self.time_not_response = 0
 			self.prev_message = []
@@ -43,15 +50,32 @@ class Talk_Automation():
 	def check_respond(self, max_time):
 		try: 
 			content = self.browser.find_elements_by_css_selector('.message.user.other')
+			last_content = content[-1].text.splitlines()[-1]
+			isResponsed = False
 			if len(self.prev_message) == 0:
-				self.prev_message.append(content[-1].text)
-			elif content[-1].text == self.prev_message[-1]:
+				if last_content != '對方正在輸入...':
+					self.prev_message.append(last_content)
+				say_hi = self.browser.find_element_by_css_selector('.main-input')
+				say_hi.send_keys('Hi')
+				say_hi.send_keys(Keys.ENTER)
+			elif last_content == self.prev_message[-1]:
 				self.time_not_response += 1
 				if self.time_not_response > 3:
 					print("Not respoding for " + str(self.time_not_response*10) + " seconds")
 			else:
-				self.prev_message.append(content[-1].text)
+				if last_content != '對方正在輸入...':
+					self.prev_message.append(last_content)
+					isResponsed = True
 				self.time_not_response = 0
+			if isResponsed and self.set_robot:
+				print(self.prev_message)
+				response_content = self.robot.get_response(self.prev_message[-1]).text
+				response = self.browser.find_element_by_css_selector('.main-input')
+				response.send_keys(response_content)
+				response.send_keys(Keys.ENTER)
+				print(response_content)
+				self.dialog[self.prev_message[-1]].add(response)
+				time.sleep(1)
 		except:
 			if len(self.prev_message) == 0:
 				self.time_not_response += 1
@@ -61,6 +85,7 @@ class Talk_Automation():
 		if self.time_not_response >= max_time * 60 // 10:
 			print("Another person not respoding for " + str(self.time_not_response*10//60) + " mins.")
 			print("Start finding new person......")
+			print(str(self.count) + ' / ' + str(self.max_people))
 			try:
 				self.browser.find_element_by_css_selector('.tool-button.leave-chat').click()
 				self.pause()
@@ -73,10 +98,35 @@ class Talk_Automation():
 			self.prev_message = []
 			self.count += 1
 
+	def check_comaand(self):
+		try:
+			content = self.browser.find_elements_by_css_selector('.message.user.me')[-1].text.splitlines()[-1]
+			if content == 'CLOSE':
+				self.close()
+			elif content == 'STOP':
+				self.set_robot = False
+			elif content == 'CHANGE':
+				try:
+					self.browser.find_element_by_css_selector('.message.user.me').click()
+					time.sleep(1)
+					self.browser.find_element_by_css_selector('.tool-button.leave-chat').click()
+					self.pause()
+					self.browser.find_element_by_css_selector('.modal-default-button.confirm').click()
+					self.pause()
+					self.start()
+				except:
+					self.start()
+				self.time_not_response = 0
+				self.prev_message = []
+				self.count += 1
+		except:
+			pass
+
 	def pause(self):
 		time.sleep(3)
 
 	def close(self):
 		print("Closing...")
 		self.browser.quit()
+		time.sleep(1)
 		exit()
--- auto_reply/main.py
+++ auto_reply/main.py
@@ -5,28 +5,61 @@ url = r"https://knock.tw"
 MAX_PEOPLE = int(input("How many people you want to meet? "))
 MAX_TIME = int(input("How much time you're willing to wait when not responding (mins)? "))
 START_MESSAGE = input("Enter your starting message: ")
+NEED_ROBOT = input("Need Robot (True/False)? ")
+MAX_ERROR = 5
+
+if NEED_ROBOT.lower() in ['true', 'yes', 'y', 'yeah', 'yup', '1']:
+	NEED_ROBOT = True
+else:
+	NEED_ROBOT = False
+
+print("")
+print("--------------------------------------------")
+print("Number of people you want to meet: {}".format(MAX_PEOPLE))
+print("Time you willing to wait for each person: {} min(s)".format(MAX_TIME))
+print("Enable Robot: {}".format(str(NEED_ROBOT)))
+print("--------------------------------------------")
+print("Basic command")
+print("CHANGE - find another person")
+print("CLOSE - close the browser")
+print("STOP - stop Robot")
+print("--------------------------------------------")
 
 print("Starting...")
-count = 1
+people_count = 1
+error_count = 1
 prev_message = []
-time_not_response = 0
-talk = Talk_Automation(url, count, prev_message, time_not_response)
+time_not_response = 0 
+talk = Talk_Automation(url, people_count, prev_message, time_not_response, MAX_PEOPLE, set_robot=NEED_ROBOT)
 time.sleep(5)
 
+
 # Start Application
-try:
-	talk.start()
-except:
-	print("Starting Error!!")
-	talk.close()
+start = False
+while not start:
+	try:
+		talk.start()
+		start = True
+	except:
+		print("Starting Error!!")
+		error_count += 1
+		time.sleep(2)
+		if error_count == MAX_ERROR:
+			talk.close()
 time.sleep(5)
 
 # Setting name
-try:
-	talk.set_name(START_MESSAGE)
-except:
-	print("Setting Starter Message Error!!")
-	talk.close()
+insert_start = False
+while not insert_start:
+	try:
+		talk.set_name(START_MESSAGE)
+		insert_start = True
+	except:
+		print("Setting Starter Message Error!!")
+		error_count += 1
+		time.sleep(2*error_count)
+		if error_count == MAX_ERROR:
+			talk.close()
 
 print("All set!")
 
@@ -39,10 +72,15 @@ while True:
 	# If someone says goodbye
 	talk.other_leave()
 
-	time.sleep(10)
+	time.sleep(5)
 
 	# If someone isn't responding
 	talk.check_respond(MAX_TIME)
+
+	time.sleep(1)
+
+	# Check command
+	talk.check_comaand()
 
 	# Check for edge cases
 	try:
